558d5f0a46efd13ffd3e1da182ed4965669e63fa
Kevin R. Thornton <krthornt@uci.edu>
2026-06-11 09:23:25 -0700

    make things pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 mod incremental_algorithm;
 mod stats;
+
+pub use stats::SingleSiteStatistic;
+pub use stats::StatsError;
+pub use stats::single_site_statistic;
--- src/stats.rs
+++ src/stats.rs
@@ -327,8 +327,6 @@ pub fn single_site_statistic<N: Iterator<Item = NodeId>, S: SingleSiteStatistic>
     statistic: S,
     ts: &TreeSequence,
 ) -> Result<S, StatsError> {
-    let mutation_parent = ts.tables().mutations().parent_column();
-    let num_edges = ts.edges().num_rows().as_usize();
     let (tree_data, num_sampled_genomes) = setup_samples(ts, samples)?;
 
     let mut sample_sets = SingleSampleSet {
